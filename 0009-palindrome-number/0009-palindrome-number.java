7be8a92c0743bdf90d0f35685cdb5e0d08ca4add
Tanishk Pillai <tanishkpillai6678@gmail.com>
2026-07-15 23:16:43 +0530

    Time: 5 ms (80.69%), Space: 45.8 MB (87.56%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) 
+    {
+        int rev=0;
+        int original=x;
+        if(x<0)
+        {
+            return false;
+        }
+        while(x!=0)
+        {
+            int digit=x%10;
+            rev=rev*10+digit;
+            x=x/10;
+        }
+        return original==rev;
+    }
+}
